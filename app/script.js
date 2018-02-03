7ec82034254578c5c79dc868f310dc3d852a764f
Agnieszka Lempaszek <agnieszka.lempaszek@student.uj.edu.pl>
2018-02-03 14:44:32 +0100

    Style ready, no js functions yet

---
+++ app/script.js
@@ -0,0 +1,11 @@
+var input = document.getElementById('res');
+
+var zero = document.getElementById('btn0');
+var one = document.getElementById('btn1');
+var clear = document.getElementById('btnClr');
+var equal = document.getElementById('btnEql');
+var sum = document.getElementById('btnSum');
+var sub = document.getElementById('btnSub');
+var mul = document.getElementById('btnMul');
+var div = document.getElementById('btnDiv');
+
